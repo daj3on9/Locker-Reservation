e23e4ffefeb74a9ee8915d1bddd7e1c5b75fb2a4
daj3on9 <dajung5623@naver.com>
2024-02-22 00:15:55 +0900

    Feat : 로그인 css 변경

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,36 +1,32 @@
-import React from 'react';
-import '../style/Login.css';
-import yu_logo from '../asset/yu_logo.svg';
-
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider, alpha, getContrastRatio } from '@mui/material/styles';
+// Login.js
+import React from "react";
+import "../style/common.css";
+import yu_logo from "../asset/yu_logo.svg";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
+import { ThemeProvider } from "@mui/material/styles";
+import buttonTheme from "../style/theme";
 
 function Login() {
-    const blueBase = '#003C84';
-    const blueMain = alpha(blueBase, 0.8);
-
-    const theme = createTheme({
-        palette: {
-            blue: {
-                main: blueMain,
-                light: alpha(blueBase, 0.5),
-                dark: alpha(blueBase, 0.9),
-                contrastText: getContrastRatio(blueMain, '#fff') > 4.5 ? '#fff' : '#111',
-            },
-        },
-    });
-
     return (
-        <div className="login">
-            <div className="login-header">
+        <div className="center-view ">
+            <div className="view-header">
                 <img src={yu_logo} alt="영남대 로고" />
-                <h2 style={{ color: '#193973' }}> 컴퓨터공학부 사물함 예약 시스템 </h2>
+                <h2 style={{ color: "#193973" }}>
+                    {" "}
+                    컴퓨터공학부 사물함 예약 시스템{" "}
+                </h2>
             </div>
-            <div className="login-container">
-                <div className="form">
-                    <TextField id="standard-basic" fullWidth label="학번" variant="standard" size="small" />
+            <div className="user-container">
+                <div className="user-form">
+                    <TextField
+                        id="standard-basic"
+                        fullWidth
+                        label="학번"
+                        variant="standard"
+                        size="small"
+                    />
                     <TextField
                         id="standard-basic"
                         fullWidth
@@ -39,13 +35,13 @@ function Login() {
                         size="small"
                         type="password"
                     />
-                    <ThemeProvider theme={theme}>
-                        <Button variant="contained" color="blue">
+                    <ThemeProvider theme={buttonTheme}>
+                        <Button variant="contained" color="primary">
                             Login
                         </Button>
                     </ThemeProvider>
                 </div>
-                <Link href="/" underline="always" color={'#213775'}>
+                <Link href="/register" underline="always" color={"#213775"}>
                     회원가입 하러 가기
                 </Link>
             </div>
