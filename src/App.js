20d884a6413ece15bb19fbdcbc13503e9b301bcc
daj3on9 <dajung5623@naver.com>
2024-02-22 00:15:29 +0900

    Feat : Router 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
-import Login from './pages/Login';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Main from "./pages/Main";
 
 function App() {
     return (
         <div className="App">
-            <Login />
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
         </div>
     );
 }
