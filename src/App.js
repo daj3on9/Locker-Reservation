2f16fce80c5aacc94235af512122f7f30753df3c
daj3on9 <dajung5623@naver.com>
2024-02-21 17:32:38 +0900

    Feat : 로그인 화면 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Login from './pages/Login';
-import Test from './pages/Test';
 
 function App() {
     return (
         <div className="App">
             <Login />
-            <Test />
         </div>
     );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,15 +3,32 @@ import '../style/Login.css';
 import yu_logo from '../asset/yu_logo.svg';
 
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
 
 function Login() {
     return (
         <div className="login">
-            <img src={yu_logo} alt="영남대 로고" />
-            <h2 style={{ color: '#193973' }}> 컴퓨터공학부 사물함 예약 시스템 </h2>
-
-            <div style={{ backgroundColor: '#ffffff', padding: '30px' }}>
-                <TextField id="filled-basic" label="Filled" variant="filled" />
+            <div className="login-header">
+                <img src={yu_logo} alt="영남대 로고" />
+                <h2 style={{ color: '#193973' }}> 컴퓨터공학부 사물함 예약 시스템 </h2>
+            </div>
+            <div className="login-container">
+                <div className="form">
+                    <TextField id="standard-basic" fullWidth label="학번" variant="standard" size="small" />
+                    <TextField
+                        id="standard-basic"
+                        fullWidth
+                        label="비밀번호"
+                        variant="standard"
+                        size="small"
+                        type="password"
+                    />
+                    <Button variant="contained">Login</Button>
+                </div>
+                <Link href="/" underline="hover">
+                    회원가입 하러 가기
+                </Link>
             </div>
         </div>
     );
