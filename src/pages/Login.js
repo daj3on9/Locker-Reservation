b2fa84c5df2271b60c869d96cd6da6f2cb73cfd8
daj3on9 <dajung5623@naver.com>
2024-02-21 16:36:10 +0900

    Feat : Login 화면 추가

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,11 +2,17 @@ import React from 'react';
 import '../style/Login.css';
 import yu_logo from '../asset/yu_logo.svg';
 
+import TextField from '@mui/material/TextField';
+
 function Login() {
     return (
-        <div>
+        <div className="login">
             <img src={yu_logo} alt="영남대 로고" />
             <h2 style={{ color: '#193973' }}> 컴퓨터공학부 사물함 예약 시스템 </h2>
+
+            <div style={{ backgroundColor: '#ffffff', padding: '30px' }}>
+                <TextField id="filled-basic" label="Filled" variant="filled" />
+            </div>
         </div>
     );
 }
